92492e6a5b50337a05b688b1a993736babaa69af
“acamposla” <alejandro.campos@imprexeurope.com>
2021-06-26 10:26:39 +0200

    Mensaje

---
+++ src/my_first_R.R
@@ -0,0 +1,33 @@
+# RStudio, setup proj, git ----------
+# Done
+# <- : opt +-
+
+
+# R basics I : vectros, lists, matrices, data.frames----------
+
+a <- 5
+b = 5
+
+#para ejecutar una línea de código puedes hacer COmmand + Enter
+# o option + Enter
+
+
+print("Hola")
+
+# La unidad básica es un vector
+
+nmrs <- seq(40)
+nmrs
+
+#vectors ~ tupla en python
+
+primer_vector = c(5, 1, 4, 6, 2, 8, 9)
+primer_vector 
+
+primer_vector + 6
+
+primer_vector + c(6 + 2)
+
+vctor_letras <- letters[1:6]
+paste(vctor_letras, LETTERS[1:6])
+
